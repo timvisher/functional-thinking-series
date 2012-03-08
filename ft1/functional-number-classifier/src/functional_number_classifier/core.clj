e612fbc01f2566d17a02c55bbfd4ea308c566fe7
Tim Visher <tim.visher@siemens.com>
2012-03-08 14:35:48 -0500

    Adding problem description to functional-number-classifier. EOM

--- ft1/functional-number-classifier/src/functional_number_classifier/core.clj
+++ ft1/functional-number-classifier/src/functional_number_classifier/core.clj
@@ -1,1 +1,8 @@
+;;; The requirements state that, given any positive integer greater
+;;; than 1, you must classify it as either perfect, abundant, or
+;;; deficient. A perfect number is a number whose factors (excluding
+;;; the number itself as a factor) add up to the number. Similarly, an
+;;; abundant number's sum of factors is greater than the number, and a
+;;; deficient number's sum of factors is less.
+
 (ns functional-number-classifier.core)
