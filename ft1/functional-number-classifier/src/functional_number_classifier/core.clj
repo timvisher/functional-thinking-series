b3dca516f71471ae63fd8adc3a9b9fadd02b1cb0
Tim Visher <tim.visher@siemens.com>
2012-03-08 14:34:46 -0500

    Initial Commit. EOM

---
+++ ft1/functional-number-classifier/src/functional_number_classifier/core.clj
@@ -0,0 +1,1 @@
+(ns functional-number-classifier.core)
---
+++ ft1/functional-number-classifier/test/functional_number_classifier/test/core.clj
@@ -0,0 +1,6 @@
+(ns functional-number-classifier.test.core
+  (:use [functional-number-classifier.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
