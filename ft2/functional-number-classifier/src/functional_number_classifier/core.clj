20aa4d4a413d653d4b549dcbcec56db8dde79daa
Tim Visher <tim.visher@siemens.com>
2012-03-16 13:05:33 -0400

    Coping ft1/functional-number-classifier to ft2. EOM

---
+++ ft2/functional-number-classifier/src/functional_number_classifier/core.clj
@@ -0,0 +1,22 @@
+;;; The requirements state that, given any positive integer greater
+;;; than 1, you must classify it as either perfect, abundant, or
+;;; deficient. A perfect number is a number whose factors (excluding
+;;; the number itself as a factor) add up to the number. Similarly, an
+;;; abundant number's sum of factors is greater than the number, and a
+;;; deficient number's sum of factors is less.
+
+(ns functional-number-classifier.core)
+
+(defn factors [number]
+  (filter #(integer? (/ number %)) (range 1 (+ 1 number))))
+
+(defn proper-factors [number]
+  (filter (partial > number) (factors number)))
+
+(defn aliquot-sum [number]
+  (reduce + (proper-factors number)))
+
+(defn classify [number]
+  (cond (= (aliquot-sum number) number) :perfect
+        (< (aliquot-sum number) number) :deficient
+        :default :abundant))
