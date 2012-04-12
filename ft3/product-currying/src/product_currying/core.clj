18cd1dd21f36bef47ddad210b63bf8823f1a7a86
Tim Visher <tim.visher@gmail.com>
2012-04-11 20:59:35 -0400

    Creating new project for currying demonstration. EOM

---
+++ ft3/product-currying/src/product_currying/core.clj
@@ -0,0 +1,1 @@
+(ns product-currying.core)
---
+++ ft3/product-currying/test/product_currying/test/core.clj
@@ -0,0 +1,6 @@
+(ns product-currying.test.core
+  (:use [product-currying.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
