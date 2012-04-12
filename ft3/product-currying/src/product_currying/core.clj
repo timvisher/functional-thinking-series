695bcfa2d844e6172b372201a1a4c505551ca36c
Tim Visher <tim.visher@gmail.com>
2012-04-11 21:03:03 -0400

    Adding comment of groovy function I'll be converting. EOM

--- ft3/product-currying/src/product_currying/core.clj
+++ ft3/product-currying/src/product_currying/core.clj
@@ -1,1 +1,9 @@
+;; def product = { x, y -> return x * y }
+
+;; def quadrate = product.curry(4)
+;; def octate = product.curry(8) 
+
+;; println "4x4: ${quadrate.call(4)}"
+;; println "5x8: ${octate(5)}"
+
 (ns product-currying.core)
