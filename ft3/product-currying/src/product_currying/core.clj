588a2351c733cee06fcd257d23c02eaab36d2c19
Tim Visher <tim.visher@gmail.com>
2012-04-11 21:05:45 -0400

    Adding comment for the next groovy code block. EOM

--- ft3/product-currying/src/product_currying/core.clj
+++ ft3/product-currying/src/product_currying/core.clj
@@ -11,3 +11,13 @@
 (def quadrate (partial * 4))
 
 (def octate (partial * 8))
+
+;; def volume = { h, w, l -> return h * w * l }
+;; def area = volume.curry(1)
+;; def lengthPA = volume.curry(1, 1) //partial application
+;; def lengthC = volume.curry(1).curry(1) // currying
+
+;; println "The volume of the 2x3x4 rectangular solid is ${volume(2, 3, 4)}"
+;; println "The area of the 3x4 rectangle is ${area(3, 4)}"
+;; println "The length of the 6 line is ${lengthPA(6)}"
+;; println "The length of the 6 line via curried function is ${lengthC(6)}"
