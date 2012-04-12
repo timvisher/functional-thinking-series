4647ed18630a645699176aa3b778a81002285dd0
Tim Visher <tim.visher@gmail.com>
2012-04-11 22:08:58 -0400

    One more time! EOM

--- ft3/product-currying/src/product_currying/core.clj
+++ ft3/product-currying/src/product_currying/core.clj
@@ -57,3 +57,17 @@
 ;;   println(filter(nums, dividesBy(3)))
 ;; }
 (defn divides-by? [divisor number] (= 0 (rem number divisor)))
+
+;; object CurryTest extends Application {
+
+;;   def filter(xs: List[Int], p: Int => Boolean): List[Int] =
+;;     if (xs.isEmpty) xs
+;;     else if (p(xs.head)) xs.head :: filter(xs.tail, p)
+;;     else filter(xs.tail, p)
+
+;;   def dividesBy(n: Int)(x: Int) = ((x % n) == 0)
+
+;;   val nums = List(1, 2, 3, 4, 5, 6, 7, 8)
+;;   println(filter(nums, dividesBy(2)))
+;;   println(filter(nums, dividesBy(3)))
+;; }
