309178e81958b5c573de558d1e4ed00edc549cd8
Tim Visher <tim.visher@gmail.com>
2012-04-11 21:26:41 -0400

    Again! EOM

--- ft3/product-currying/src/product_currying/core.clj
+++ ft3/product-currying/src/product_currying/core.clj
@@ -42,3 +42,17 @@
 
 ;; println "increment 7: ${incrementer(7)}"
 (def increment (partial + 1))
+
+;; object CurryTest extends Application {
+
+;;   def filter(xs: List[Int], p: Int => Boolean): List[Int] =
+;;     if (xs.isEmpty) xs
+;;     else if (p(xs.head)) xs.head :: filter(xs.tail, p)
+;;     else filter(xs.tail, p)
+
+;;   def dividesBy(n: Int)(x: Int) = ((x % n) == 0)
+
+;;   val nums = List(1, 2, 3, 4, 5, 6, 7, 8)
+;;   println(filter(nums, dividesBy(2)))
+;;   println(filter(nums, dividesBy(3)))
+;; }
