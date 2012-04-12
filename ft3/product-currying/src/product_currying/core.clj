2ae170d85bce6e59aaf2681d0c0345bcab7752da
Tim Visher <tim.visher@gmail.com>
2012-04-11 22:49:25 -0400

    Moving ns declaration. EOM

--- ft3/product-currying/src/product_currying/core.clj
+++ ft3/product-currying/src/product_currying/core.clj
@@ -1,3 +1,5 @@
+(ns product-currying.core)
+
 ;; def product = { x, y -> return x * y }
 
 ;; def quadrate = product.curry(4)
@@ -5,8 +7,6 @@
 
 ;; println "4x4: ${quadrate.call(4)}"
 ;; println "5x8: ${octate(5)}"
-
-(ns product-currying.core)
 
 (def quadrate (partial * 4))
 
